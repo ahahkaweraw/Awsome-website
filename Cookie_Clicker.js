728941dba95ec06136d46576bb210fa54d35b89a
Kwert <127696562+ahahkaweraw@users.noreply.github.com>
2024-10-11 20:48:15 -0500

    Update Cookie_Clicker.js
    UPDATE 2.0

--- Cookie_Clicker.js
+++ Cookie_Clicker.js
@@ -1,5 +1,17 @@
 let Cookie = 0;
+let MCookie
 let Clicker = document.getElementById("Clicker").onclick = function(){
     Cookie+=1;
-    document.getElementById("Counter").innerHTML = "You have " + Cookie + " cookies"
-}   
+    MCookie = Cookie**2
+    console.log(Cookie)
+    if(MCookie <= 1000000) {
+        document.getElementById("Counter").innerHTML = "You have " + MCookie + " cookies."
+        document.getElementById("p4").innerHTML = "They taste awful but this is just the beginning."
+    } else if(MCookie >= 1000000 && MCookie>= 100000000000) {
+        document.getElementById("Counter").innerHTML = "You have " + MCookie.toExponential(2) + " cookies."
+        document.getElementById("p4").innerHTML = "You're cookies are improving."
+    } else if(MCookie >= 10000000000) {
+        document.getElementById("Counter").innerHTML = "You have " + MCookie.toExponential(2) + " cookies."
+        document.getElementById("p4").innerHTML = "You're cookies taste AMAZING!!! Everyone in the town are enjoying your cookies!"
+    }
+}
